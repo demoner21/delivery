acc44d690666ef5062f87b3f2d942e659ffd79e7
demoner21 <a.tranhago@gmail.com>
2026-09-17 22:23:08 -0300

    fix: validator regex

--- src/main/java/com/deliverytech/delivery_api/validation/CEPValidator.java
+++ src/main/java/com/deliverytech/delivery_api/validation/CEPValidator.java
@@ -4,24 +4,24 @@ import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import java.util.regex.Pattern;
 
-public class CEPValidator implements ConstraintValidator<ValidCEP, String>{
+public class CEPValidator implements ConstraintValidator<ValidCEP, String> {
 
     private static final Pattern CEP_PATTERN =
-    Pattern.compile("^([0-9]{5}-?([0-9]{3})$");
+        Pattern.compile("^[0-9]{5}-?[0-9]{3}$");
 
     @Override
     public void initialize(ValidCEP constraintAnnotaion) {
-        // inicialização se necessãrio
+        // inicialização se necessário
     }
 
-     @Override
-     public boolean isValid(String cep, ConstraintValidatorContext context) {
+    @Override
+    public boolean isValid(String cep, ConstraintValidatorContext context) {
         if (cep == null || cep.trim().isEmpty()) {
             return false;
         }
 
-        String cleanCep = cep.trim().replaceAll("\s", "");
+        String cleanCep = cep.trim().replaceAll("\\s", "");
         return CEP_PATTERN.matcher(cleanCep).matches();
-     }
+    }
 
 }
